bd82dc12c92998dbce8e4626d4c6cbb74794031a
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-04-20 16:15:39 +0300

    first try? 46 errors/ simple recurtion

--- Binary Tree Traversal/binary.py
+++ Binary Tree Traversal/binary.py
@@ -1,13 +1,26 @@
 '''Binary Tree Traversal'''
 
-# # Pre-order traversal
-# def pre_order(node):
-#     return []
+# Pre-order traversal
+def pre_order(node):
+    '''pre'''
+    if not node:
+        return []
 
-# # In-order traversal
-# def in_order(node):
-#     return []
+    return [node.data] + pre_order(node.left) + pre_order(node.left)
 
-# # Post-order traversal
-# def post_order(node):
-#     return []
+
+# In-order traversal
+def in_order(node):
+    '''in'''
+    if not node:
+        return []
+
+    return in_order(node.left) + [node.data] + in_order(node.right)
+
+# Post-order traversal
+def post_order(node):
+    '''post'''
+    if node is None:
+        return []
+
+    return post_order(node.left) + post_order(node.right) + [node.data]
