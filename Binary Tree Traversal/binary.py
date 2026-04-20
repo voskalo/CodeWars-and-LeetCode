9ac679995d55c3fab4fa170d59078182f24268c1
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-04-20 16:12:18 +0300

    first set up, and preparation

---
+++ Binary Tree Traversal/binary.py
@@ -0,0 +1,13 @@
+'''Binary Tree Traversal'''
+
+# # Pre-order traversal
+# def pre_order(node):
+#     return []
+
+# # In-order traversal
+# def in_order(node):
+#     return []
+
+# # Post-order traversal
+# def post_order(node):
+#     return []
