87cb22953ce454c6b3f75c261c9eafffc91a2803
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-04-20 19:33:55 +0300

    first try passed, wrote the main fucnt using deque, adding tests_passed screenshot

--- Sort binary tree by levels/sort.py
+++ Sort binary tree by levels/sort.py
@@ -1,10 +1,30 @@
 '''Sort binary tree by levels'''
+from collections import deque
 
 class Node:
+    '''node cls'''
     def __init__(self, L, R, n):
+        '''init'''
         self.left = L
         self.right = R
         self.value = n
 
-def tree_by_levels(node):
-    return
+def tree_by_levels(node: 'Node'):
+    '''main funct'''
+
+    if not node:
+        return []
+
+    res = []
+    dq = deque([node])
+    while dq:
+        cur = dq.popleft()
+        res.append(cur.value)
+
+        if cur.left:
+            dq.append(cur.left)
+
+        if cur.right:
+            dq.append(cur.right)
+
+    return res
